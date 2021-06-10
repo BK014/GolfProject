73bf3d2a8d6afcc7671aa06f4428040986183fc4
BK014 <simmikaur297@gmail.com>
2021-06-11 10:04:50 +1200

    Add project files.

---
+++ GolfProject/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace GolfProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvGolf = new System.Windows.Forms.DataGridView();
+            this.btnLoadDB = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.txtDOB = new System.Windows.Forms.TextBox();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtSuburb = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtAvailable = new System.Windows.Forms.TextBox();
+            this.txtHandicap = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGolf)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvGolf
+            // 
+            this.dgvGolf.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGolf.Location = new System.Drawing.Point(147, 12);
+            this.dgvGolf.Name = "dgvGolf";
+            this.dgvGolf.Size = new System.Drawing.Size(411, 269);
+            this.dgvGolf.TabIndex = 0;
+            this.dgvGolf.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvGolf_CellContentClick);
+            this.dgvGolf.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvGolf_CellContentClick);
+            // 
+            // btnLoadDB
+            // 
+            this.btnLoadDB.Location = new System.Drawing.Point(26, 52);
+            this.btnLoadDB.Name = "btnLoadDB";
+            this.btnLoadDB.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadDB.TabIndex = 1;
+            this.btnLoadDB.Text = "Load DB";
+            this.btnLoadDB.UseVisualStyleBackColor = true;
+            this.btnLoadDB.Click += new System.EventHandler(this.btnLoadDB_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(12, 310);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Location = new System.Drawing.Point(147, 310);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstname.TabIndex = 3;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(283, 310);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 4;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(417, 310);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 5;
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(551, 310);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(100, 20);
+            this.txtGender.TabIndex = 6;
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.Location = new System.Drawing.Point(671, 310);
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.Size = new System.Drawing.Size(100, 20);
+            this.txtDOB.TabIndex = 7;
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(12, 355);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(100, 20);
+            this.txtStreet.TabIndex = 8;
+            // 
+            // txtSuburb
+            // 
+            this.txtSuburb.Location = new System.Drawing.Point(147, 355);
+            this.txtSuburb.Name = "txtSuburb";
+            this.txtSuburb.Size = new System.Drawing.Size(100, 20);
+            this.txtSuburb.TabIndex = 9;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(283, 355);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 10;
+            // 
+            // txtAvailable
+            // 
+            this.txtAvailable.Location = new System.Drawing.Point(417, 355);
+            this.txtAvailable.Name = "txtAvailable";
+            this.txtAvailable.Size = new System.Drawing.Size(100, 20);
+            this.txtAvailable.TabIndex = 11;
+            // 
+            // txtHandicap
+            // 
+            this.txtHandicap.Location = new System.Drawing.Point(551, 355);
+            this.txtHandicap.Name = "txtHandicap";
+            this.txtHandicap.Size = new System.Drawing.Size(100, 20);
+            this.txtHandicap.TabIndex = 12;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(649, 25);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 13;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(649, 89);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(649, 160);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtHandicap);
+            this.Controls.Add(this.txtAvailable);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtSuburb);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtFirstname);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnLoadDB);
+            this.Controls.Add(this.dgvGolf);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGolf)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvGolf;
+        private System.Windows.Forms.Button btnLoadDB;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.TextBox txtDOB;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtSuburb;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtAvailable;
+        private System.Windows.Forms.TextBox txtHandicap;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ GolfProject/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GolfProject
+{
+    public partial class Form1 : Form
+    {
+        //connection string
+        private string connectionString = @"Data Source=LAPTOP-AAPS5QSM\SQLEXPRESS;Initial Catalog=Golf;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
+        SqlConnection Con = new SqlConnection();
+        DataTable GolfTable = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            Con.ConnectionString = connectionString;
+        }
+
+        private void btnLoadDB_Click(object sender, EventArgs e)
+        {
+            loaddb();
+
+        }
+
+        private void loaddb()
+        {
+            //load datatable colums
+            datatablecolumns();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string QueryString = @"SELECT * FROM Golf order by ID";
+                //open your connection 
+                connection.Open();
+
+                SqlCommand Command = new SqlCommand(QueryString, connection);
+                //start your DB reader
+
+                SqlDataReader reader = Command.ExecuteReader();
+
+                while (reader.Read()) //looping through data
+                {
+                    //add in each row to the datatable
+                    GolfTable.Rows.Add(
+                        reader["ID"],
+                        reader["Title"],
+                        reader["Firstname"],
+                        reader["Surname"],
+                        reader["Gender"],
+                        reader["DOB"],
+                        reader["Street"],
+                        reader["Suburb"],
+                        reader["City"],
+                        reader["Available week days"],
+                        reader["Handicap"]);
+
+                }
+                reader.Close();//close reader
+                connection.Close();//close connection
+                // add the datatable into your data grid view
+                dgvGolf.DataSource = GolfTable;
+
+            }
+        }
+
+        private void datatablecolumns()
+        {
+
+            //clear the old data
+            GolfTable.Clear();
+            //add in the column titlees to the datable
+            try
+            {
+                GolfTable.Columns.Add("ID");
+                GolfTable.Columns.Add("Title");
+                GolfTable.Columns.Add("Firstname");
+                GolfTable.Columns.Add("Surname");
+                GolfTable.Columns.Add("Gender");
+                GolfTable.Columns.Add("DOB");
+                GolfTable.Columns.Add("Street");
+                GolfTable.Columns.Add("Suburb");
+                GolfTable.Columns.Add("City");
+                GolfTable.Columns.Add("Availavle week days");
+                GolfTable.Columns.Add("Handicap");
+
+            }
+            catch (Exception s)
+            {
+                //MessageBox.Show("Data Table not loading"+ s.Message);
+            }
+        }
+
+        private void dgvGolf_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                //get the value of clicked cell
+                string newvalue = dgvGolf.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+                //show the value on the header
+                this.Text = "Row :" + e.RowIndex.ToString() + " Col : " + e.ColumnIndex.ToString() + " Value =" + newvalue;
+
+                //fill textboxes with the data
+                txtID.Text = dgvGolf.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtTitle.Text = dgvGolf.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtFirstname.Text = dgvGolf.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtSurname.Text = dgvGolf.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtGender.Text = dgvGolf.Rows[e.RowIndex].Cells[4].Value.ToString();
+                txtDOB.Text = dgvGolf.Rows[e.RowIndex].Cells[5].Value.ToString();
+                txtStreet.Text = dgvGolf.Rows[e.RowIndex].Cells[6].Value.ToString();
+                txtSuburb.Text = dgvGolf.Rows[e.RowIndex].Cells[7].Value.ToString();
+                txtCity.Text = dgvGolf.Rows[e.RowIndex].Cells[8].Value.ToString();
+                txtAvailable.Text = dgvGolf.Rows[e.RowIndex].Cells[9].Value.ToString();
+                txtHandicap.Text = dgvGolf.Rows[e.RowIndex].Cells[10].Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // this puts the parameters into the code so that the data in the text boxes is added to the database
+            string NewEntry = "INSERT INTO Golf (Title, Firstname, Surname, Gender, DOB, Street, Suburb, City, [Available week days], Handicap) VALUES(@Title, @Firstname, @Surname, @Gender, @DOB, @Street, @Suburb, @City, @Available,@Handicap)";
+            using (SqlCommand newdata = new SqlCommand(NewEntry, Con))
+            {
+
+                newdata.Parameters.AddWithValue("@Title", txtTitle.Text);
+                newdata.Parameters.AddWithValue("@Firstname", txtFirstname.Text);
+                newdata.Parameters.AddWithValue("@Surname", txtSurname.Text);
+                newdata.Parameters.AddWithValue("@Street", txtStreet.Text);
+                newdata.Parameters.AddWithValue("@Suburb", txtSuburb.Text);
+                newdata.Parameters.AddWithValue("@City", txtCity.Text);
+                newdata.Parameters.AddWithValue("@Gender", txtGender.Text);
+                newdata.Parameters.Add("@DOB", SqlDbType.DateTime).Value =txtDOB.Text;
+                newdata.Parameters.AddWithValue("@Handicap", txtHandicap.Text);
+                newdata.Parameters.AddWithValue("@Available", txtAvailable.Text);
+                Con.Open(); //open a connection to the database
+                            //its a NONQuery as it doesn't return any data its only going up to the server
+                newdata.ExecuteNonQuery(); //Run the Query
+                Con.Close(); //Close a connection to the database
+                             //a happy message box23 May 2019 Introduction to C#.Net
+                MessageBox.Show("Data has been Inserted !! ");
+            }
+            //Run the LoadDatabase method we made earler to see the new data.
+            loaddb();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // this puts the parameters into the code so that the data in the text boxes is added to the database
+            
+            string updatestatement = "UPDATE Golf set Title=@Title, Firstname=@Firstname, Surname=@Surname, Gender = @Gender, DOB = @DOB, Street = @Street, Suburb = @Suburb, City = @City, [Available week days]= @Available, Handicap = @Handicap where ID = @ID";
+
+
+            using (SqlCommand update = new SqlCommand(updatestatement, Con))
+            {
+                update.Parameters.AddWithValue("@ID", txtID.Text);
+
+                update.Parameters.AddWithValue("@Title", txtTitle.Text);
+                update.Parameters.AddWithValue("@Firstname", txtFirstname.Text);
+                update.Parameters.AddWithValue("@Surname", txtSurname.Text);
+                update.Parameters.AddWithValue("@Street", txtStreet.Text);
+                update.Parameters.AddWithValue("@Suburb", txtSuburb.Text);
+                update.Parameters.AddWithValue("@City", txtCity.Text);
+                update.Parameters.AddWithValue("@Gender", txtGender.Text);
+                update.Parameters.Add("@DOB", SqlDbType.DateTime).Value = txtDOB.Text;
+                update.Parameters.AddWithValue("@Handicap", txtHandicap.Text);
+                update.Parameters.AddWithValue("@Available", txtAvailable.Text);
+                Con.Open(); //open a connection to the database
+                            //its a NONQuery as it doesn't return any data its only going up to the server
+                update.ExecuteNonQuery(); //Run the Query
+                Con.Close(); //Close a connection to the database
+                             //a happy message box23 May 2019 Introduction to C#.Net
+                MessageBox.Show("Data has been updated!! ");
+            }
+            //Run the LoadDatabase method we made earler to see the new data.
+            loaddb();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string DeleteCommand = "Delete Golf where ID = @ID";
+            SqlCommand DeleteData = new SqlCommand(DeleteCommand, Con);
+            DeleteData.Parameters.AddWithValue("@ID", txtID.Text);
+            Con.Open();
+            DeleteData.ExecuteNonQuery();
+            Con.Close();
+            MessageBox.Show("Data has been deleted!! ");
+            loaddb();
+
+        }
+    }
+       
+}
